9d0d8cbea90fc3ff9a28ef9116aac9c3015b9298
k549at <k549at@gmail.com>
2016-03-10 16:13:27 +0400

    guni

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+suddo ln -s /home/box/web/etc/myguni.conf /etc/gunicorn.d/
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo gunicorn -c hello.py hello:app
