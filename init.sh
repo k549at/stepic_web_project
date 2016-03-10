770f4f0087cd3e014c5d59ea78e554ecc169e21f
k549at <k549at@gmail.com>
2016-03-10 17:05:35 +0400

    hhh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-suddo ln -s /home/box/web/etc/myguni.conf /etc/gunicorn.d/
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
