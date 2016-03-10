f011e792e98868d062eaaf5ec4baa33314991c3e
k549at <k549at@gmail.com>
2016-03-10 15:55:33 +0400

    hello

---
+++ hello.py
@@ -0,0 +1,9 @@
+def app(environ, start_response):
+   query = environ.get('QUERY_STRING')
+   body = query.replace('&','\n')
+   status = '200 OK'
+   headers = [
+      ('Content-Type','text/plain')
+   ]
+start_response(status, headers)
+return [body]
