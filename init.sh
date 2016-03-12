a315784702c4ac85731a5ecbced2b6798f446c9c
k549at <k549at@gmail.com>
2016-03-12 22:13:39 +0300

    "config gunicorn"

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-
 sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/gunicorn restart
+sudo gunicorn  -c /etc/gunicorn.d/test -D hello:app
