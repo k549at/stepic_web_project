157233ad5bc35b75484713bc848a7f12cf054d3e
k549at <k549at@gmail.com>
2016-03-16 18:01:21 +0300

    create db

--- createDB.sh
+++ createDB.sh
@@ -1,7 +1,8 @@
 mysql -uroot -e "create database if not exists stepic_db"
-mysql -uroot -e "CREATE USER if not exists 'box'@'localhost' IDENTIFIED BY 'password';"
+mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'password';"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'box'@'localhost';"
 mysql -uroot -e "FLUSH PRIVILEGES;"
 
-bash ~/web/createDB.sh
+sudo ~/web/ask/manage.py syncdb
+
 
