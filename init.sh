ef419404c1d8b517164a71b26476868739eb9836
k549at <k549at@gmail.com>
2016-03-19 09:08:40 +0300

    new gunicor

--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -s ~/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-sudo gunicorn  -c /etc/gunicorn.d/test -D hello:app
+#sudo gunicorn  -c /etc/gunicorn.d/test -D hello:app
+sudo gunicorn -b 0.0.0.0:8000 ask.wsgi
