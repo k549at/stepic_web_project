12a92ec8c82bfae2a68eb54903a57d5389733c97
k549at <k549at@gmail.com>
2016-03-22 20:22:33 +0300

    tttt

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.http import Http404
 
-# Create your models here.
+#  Create your models here.
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
