2e445289e00164fac6ff5c11b63c44e3363d928b
k549at <k549at@gmail.com>
2016-03-22 17:29:49 +0300

    correct

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,7 +6,7 @@ from django.http import Http404
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(blank=False)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
