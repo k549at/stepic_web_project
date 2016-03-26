aa1c862dda10145a93081b87cc7d143a984c0271
k549at <k549at@gmail.com>
2016-03-26 08:27:40 +0300

    add answer

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -110,8 +110,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/box/web/ask/tmp',
-    '/home/box/web/ask/templ',
+    '/home/android/web/ask/tmp',
+    '/home/android/web/ask/templ',
 )
 
 INSTALLED_APPS = (
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,9 +1,12 @@
 from models import Question
 from models import Answer
+from forms import AskForm
+from forms import AnswerForm
 from django.http import HttpResponse, Http404
 from django.template import Context, loader
 from django.shortcuts import render, render_to_response
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 
 def index(request):
     tmp = loader.get_template('index.html')
@@ -35,16 +38,52 @@ def popular(request):
     return render(request,'popular.html',{'questions': page.object_list, 'paginator': paginator, 'page': page,});
 
 def question(request, *args, **kwargs):
-    templ = loader.get_template('question.html')
-    context = Context()
     quest_id = kwargs['pk']
     try:
         question = Question.objects.get(id=quest_id)
-        #answers = Answer.objects.all()
         answers = question.answer_set.all()
         answers = answers.values('text')
-        
     except Question.DoesNotExist:
         raise Http404
-   # return HttpResponse(templ.render(context));
-    return render(request, 'question.html', {'quest_title': question.title, 'quest_text': question.text, 'answers': answers,})
+    return render(request, 'question_old.html', {'quest_title': question.title, 'quest_text': question.text, 'answers': answers,})
+
+def ask_add(request):
+    if request.method == "POST":
+        form=AskForm(request.POST)
+        if form.is_valid():
+            question = form.save()
+            redir_url = '/question/' + str(question.id)
+        return HttpResponseRedirect(redir_url) 
+    else:
+        form = AskForm()        
+    return render(request, 'ask_add.html', {'form': form})
+
+def answer_add(request, *args, **kwargs):
+    quest_id = kwargs['pk']
+    try:
+        question = Question.objects.get(id=quest_id)
+        if request.method == "POST":
+            form = AnswerForm(request.POST)
+            #print 'answer add post'
+            if form.is_valid():
+                #print "valid"
+#                answer = form.save(commit=False)
+#                answer.question = quest_id
+#                answer.save()
+                form.save()
+                redir_url = '/question/'+str(quest_id)
+            return HttpResponseRedirect(redir_url) 
+        else:
+#        print 'before form:' + str(quest_id)
+            form = AnswerForm(initial={'question': question})
+
+    
+        print 'forms:'
+        print form
+        #form.question = quest_id
+        answers = question.answer_set.all()
+        answers = answers.values('text')        
+    except Question.DoesNotExist:
+        raise Http404
+    print form
+    return render(request, 'question_old.html', {'form': form, 'quest_title': question.title, 'quest_text': question.text, 'answers':answers,})
